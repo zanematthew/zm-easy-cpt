8ffee28d857b18514cd9c43b8eabca1a83120cc1
Zane M. Kolnik <zanematthew@gmail.com>
2013-05-06 20:48:05 -0400

    removing code

--- plugin.php
+++ plugin.php
@@ -10,36 +10,5 @@
  */
 
 
-define( 'ZM_EASY_CPT_VERSION', '0.1-beta' );
-define( 'ZM_EASY_CPT_OPTION', 'zm_easy_cpt_version' );
-
-ini_set('display_errors', 'on');
-error_reporting( E_ALL );
-
 require_once 'functions.php';
 require_once 'abstract.php';
-
-/**
- * When the plugin is activated we check if there is a previously
- * installed version. If there is we do nothing but return. If not
- * we update the version number.
- */
-function zm_easy_cpt_activation() {
-
-    if ( get_option( ZM_EASY_CPT_OPTION ) &&
-         get_option( ZM_EASY_CPT_OPTION ) > ZM_EASY_CPT_VERSION ){
-        return;
-    }
-
-    update_option( ZM_EASY_CPT_OPTION, ZM_EASY_CPT_VERSION );
-}
-register_activation_hook( __FILE__, 'zm_easy_cpt_activation' );
-
-
-/**
- * When the plugin is deactivated we remove our version number.
- */
-function zm_easy_cpt_deactivation(){
-    delete_option( ZM_EASY_CPT_OPTION );
-}
-register_deactivation_hook( __FILE__, 'zm_easy_cpt_deactivation' );
