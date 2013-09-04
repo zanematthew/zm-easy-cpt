3af34da67ab0ef3a201d98d1534b8e86ec1d0644
Zane M. Kolnik <zanematthew@gmail.com>
2013-09-03 22:52:08 -0400

    start of a class

--- functions.php
+++ functions.php
@@ -251,101 +251,54 @@ function zm_base_build_options( $taxonomy=null, $value=null ) {
  *
  * @param $dir the full path the plugin.
  */
-function zm_easy_cpt_reqiure( $dir=null ){
+Class zMCore {
+    public function zm_easy_cpt_reqiure( $dir=null ){
 
-    /**
-     * Read the contents of the directory into an array.
-     */
-    $tmp_controllers = scandir( $dir . 'controllers/' );
+        /**
+         * Read the contents of the directory into an array.
+         */
+        $tmp_controllers = scandir( $dir . 'controllers/' );
 
-    /**
-     * This is our list of items to ignore from the scaned directory
-     */
-    $ignore = array(
-        '.',
-        '..',
-        '.DS_Store'
-        );
+        /**
+         * This is our list of items to ignore from the scaned directory
+         */
+        $ignore = array(
+            '.',
+            '..',
+            '.DS_Store'
+            );
 
-    /**
-     * Search our array for each item in the ignore list.
-     * Since our list is indexed, we use array search, which returns
-     * the index, i.e., 0, 1, 2, etc. From here we "unset" our value.
-     * Thus removing the ignored file from the scanned directory array.
-     */
-    foreach( $ignore as $file ) {
-        $ds = array_search( $file, $tmp_controllers );
-        if ( ! is_null( $ds ) ){
-            unset( $tmp_controllers[$ds] );
-        }
-    }
-
-    /**
-     *This loop performs a require once on each item in our functions
-     * array. Once each item is loaded we split the items in the array on
-     * an "_" and use the first part of item as the file name of our post type,
-     * thus performing a require once on our post type.
-     */
-    $models = array();
-    foreach( $tmp_controllers as $controller ) {
-        require_once $dir . 'controllers/'.$controller;
-
-        $model = array_shift( explode( '_', $controller ) );
-        $models[] = $model;
-        require_once $dir . 'models/'.$model . '.php';
-    }
-}
-
-
-/**
- * Systematically creat the asset files needed for your post_type.
- * Usage: just run the function once, it should create the following
- * files for you:
- *
- * Example: post type: "contacts"
- * my-contact-plugin/assets/contacts.js
- * my-contact-plugin/assets/contacts_admin.js
- * my-contact-plugin/assets/contacts.css
- * my-contact-plugin/assets/contacts_admin.css
- */
-function zm_create_assets( $models=null, $dir=null ){
-
-    if ( ! is_dir( $dir . 'assets' ) ){
-        if ( ! wp_mkdir_p( $dir . 'assets' ) )
-            wp_die("Couldn't make assets dir, don't run the action or make the dir writeable");
-    }
-
-    $assets_dir = $dir . 'assets/';
-
-    foreach( $models as $model ){
-        $date = date('F j, Y, g:i a');
-        $files = array(
-            array(
-                'file' => $assets_dir . $model . '_admin.css',
-                'desc' => "/* \nThis file is automatically created for you. \n It is your Admin CSS file for the {$model} model. \n\nCreated On: {$date} */"
-            ),
-            array(
-                'file' => $assets_dir . $model . '.css',
-                'desc' => "/* \nThis file is automatically created for you. \n It is your CSS file for the {$model} model. Do NOT place admin styling here, instead use the {$model}_admin.css file. \n\nCreated On: {$date} */"
-            ),
-            array(
-                'file' => $assets_dir . $model . '_admin.js',
-                'desc' => "/* \nThis file is automatically created for you. \n It is your Admin JS file for the {$model} model. \n\nCreated On: {$date} */"
-            ),
-            array(
-                'file' => $assets_dir . $model . '.js',
-                'desc' => "/* \nThis file is automatically created for you. \n It is your JS file for the {$model} model. Do NOT place admin JS here, instead use the {$model}_admin.js file. \n\nCreated On: {$date} */"
-            )
-        );
-
-        foreach( $files as $file ){
-            if ( ! file_exists( $file['file'] ) ){
-                file_put_contents( $file['file'], $file['desc'] );
+        /**
+         * Search our array for each item in the ignore list.
+         * Since our list is indexed, we use array search, which returns
+         * the index, i.e., 0, 1, 2, etc. From here we "unset" our value.
+         * Thus removing the ignored file from the scanned directory array.
+         */
+        foreach( $ignore as $file ) {
+            $ds = array_search( $file, $tmp_controllers );
+            if ( ! is_null( $ds ) ){
+                unset( $tmp_controllers[$ds] );
             }
         }
+
+        /**
+         *This loop performs a require once on each item in our functions
+         * array. Once each item is loaded we split the items in the array on
+         * an "_" and use the first part of item as the file name of our post type,
+         * thus performing a require once on our post type.
+         */
+        $models = array();
+        foreach( $tmp_controllers as $controller ) {
+            require_once $dir . 'controllers/'.$controller;
+
+            $model = array_shift( explode( '_', $controller ) );
+            $models[] = $model;
+            require_once $dir . 'models/'.$model . '.php';
+        }
     }
 }
-add_action('zm_easy_cpt_create_assets','zm_create_assets', 99, 2);
+
+
 
 
 /**
